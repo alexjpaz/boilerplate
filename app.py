a879a25f0b5c5aac04457594247e42a2f1af25d0
Alex Paz <alexander.j.paz@ngc.com>
2014-05-06 14:20:15 -0400

    main metho, logger

---
+++ app.py
@@ -0,0 +1,8 @@
+from bp import logger
+
+log = logger.get(__name__)
+
+def main():
+	log.info('main method')
+
+if  __name__ =='__main__':main()
---
+++ bp/logger.py
@@ -0,0 +1,6 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+def get(name):
+		return logging.getLogger(name)
